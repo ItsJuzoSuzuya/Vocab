bad9b74afe47ab9a986417de4db526a964d566b1
Nick Schefner <124909285+ItsJuzoSuzuya@users.noreply.github.com>
2023-11-20 02:14:00 +0100

    fixed invisivility of php-fpm by adding nginx server and connecting it

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,7 @@
-FROM php:8.1-fpm as php_stage
+# Dockerfile for PHP-FPM
+FROM php:8.3-rc-fpm
 
-# Installing dependencies for the PHP modules
-RUN apt-get update && \
-    apt-get install -y zip curl libcurl3-dev libzip-dev libpng-dev libonig-dev libxml2-dev && \
-    rm -rf /var/lib/apt/lists/*
+# Install required extensions
+RUN docker-php-ext-install mysqli pdo pdo_mysql
 
-# Installing additional PHP modules
-RUN docker-php-ext-install curl gd mbstring mysqli pdo pdo_mysql xml
-
-# Install and configure ImageMagick
-RUN apt-get update && \
-    apt-get install -y libmagickwand-dev && \
-    pecl install imagick && \
-    docker-php-ext-enable imagick && \
-    apt-get purge -y libmagickwand-dev && \
-    rm -rf /var/lib/apt/lists/*
-
-# Install Composer so it's available
-RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
-# Expose the port your PHP application will run on
-EXPOSE 9000
+CMD ["php-fpm"]
--- databases/db.php
+++ databases/db.php
@@ -1,24 +1,39 @@
-<?php header("Access-Control-Allow-Origin: *");
-
+<?php
+header("Access-Control-Allow-Origin: *");
 include "./define.php";
 
-$db = new mysqli( HOST, USER, PASSWORD, DB_NAME,PORT);
-if (isset($_POST["language"])) {
-    echo 'Fresh';
-    $selectedLanguage = $_POST["language"];
-    $sql = "INSERT INTO languages(language)
-            SELECT '$selectedLanguage'
-            WHERE NOT EXISTS (SELECT * FROM languages WHERE language = '$selectedLanguage')";
+$db = new mysqli(HOST, USER, PASSWORD, DB_NAME, PORT);
 
-    $db->query($sql);
-
-    // You can perform any necessary PHP logic here based on the selected language
-    // For example, save the language to a database or perform other operations
-
-    // Respond with a success status (HTTP 200)
-    http_response_code(200);
-} else {
-    // Respond with a bad request status (HTTP 400) if language parameter is missing
-    http_response_code(400);
-    echo "Bad request. Language parameter";
+// Check for errors in database connection
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // This is a preflight request, respond with the allowed methods and headers
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Allow-Headers: Content-Type");
+    http_response_code(204);
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST["language"])) {
+    $selectedLanguage = $_POST["language"];
+
+    // Use prepared statement to prevent SQL injection
+    $stmt = $db->prepare("INSERT INTO languages(language) SELECT ? WHERE NOT EXISTS (SELECT * FROM languages WHERE language = ?)");
+    $stmt->bind_param("ss", $selectedLanguage, $selectedLanguage);
+    $stmt->execute();
+
+    // Check for errors in the SQL execution
+    if ($stmt->error) {
+        echo 'Error: ' . $stmt->error;
+        http_response_code(500); // Internal Server Error
+    } else {
+        echo 'Fresh';
+        http_response_code(200);
+    }
+
+    $stmt->close(); // Close the prepared statement
+} else {
+    http_response_code(404);
+}
+
+$db->close(); // Close the database connection
